b5a3043a147d16c3b2583fefd9bbadd9efc80a28
LeonardoMoreira7 <shark15114@sharkcoders.pt>
2026-03-20 20:36:04 +0000

    Add files via upload

--- exercício30.py
+++ exercício30.py
@@ -1,19 +1,33 @@
-
 from random import randint
 from time import sleep
-li = []
+saldo = randint(100,200)
 print("Bem vindo ao Slot Machine!\n 3 números iguais = Aposta x 50\n 2 números iguais = Aposta x 3\n 3 números diferentes = Perde o valor apostado")
 while True:
+    li = []
     for x in range(3):
         n = randint(1,6)
         li.append(n)
-    saldo = randint(100,200)
     print(f"O seu saldo atual é de {saldo} euros")
 
     montante = int(input("Digite o montante que deseja apostar: "))
     if montante <= 0:
         print("Montante inválido")
     else:
-        print("Aperte ENTER para iniciar")
+        input('Aperte ENTER para iniciar')
         print("Girando ...")
         sleep (3)
+        for x in li:
+            print([x], end='')
+            sleep(2)
+        if li[0] == li[1] or li[0] == li[2] or li [1] == li[2]:
+            saldo = montante * 3
+            print()
+            print("Acertou 2!")
+        elif li[0] == li[1] == li[2]:
+            saldo = montante * 50
+            print()
+            print("JAKPOT!")
+        else:
+            saldo = saldo - montante
+            print()
+            print("Perdeu, tente outra vez!")
---
+++ exercício31.py
@@ -0,0 +1,36 @@
+from random import randint
+saldo = randint(100,200)
+cartas = randint(2,10)
+banca = 0
+while True:
+    print(f"O seu saldo atual é de {saldo} euros")
+    montante = int(input("Digite o montante que deseja apostar: "))
+    print(f"Tens {cartas} pontos")
+    while True:
+        opc = int(input("Deseja comprar mais cartas?"))
+        if opc == 1:
+            cartas += randint(2, 10)
+            print(f"Estás com {cartas} pontos\n")
+            if cartas > 21:
+                print()
+                print("Perdeste, tenta outra vez!")
+                break
+        elif opc == 2:
+            banca = randint(15,21)
+            print(f"A banca somou {banca} pontos")
+        if cartas > banca:
+            print()
+            ganhos = montante * 2
+            saldo += ganhos
+            print("Ganhaste!")
+            print(f"O saldo atual é {saldo} euros")
+        elif cartas < banca:
+            print()
+            saldo = saldo - montante
+            print("Perdeste!")
+            print(f"O saldo atual é {saldo} euros")
+            break
+        else:
+            print()
+            print("Foi um empate!")
+            break
